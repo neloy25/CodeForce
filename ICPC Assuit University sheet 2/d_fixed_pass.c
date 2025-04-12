1b975fe838c7d660e5b031df3d446ccae9966ef5
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-12 16:20:40 +0600

    ICPC LOOP D: Fixed Password

---
+++ ICPC Assuit University sheet 2/d_fixed_pass.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    while(1){
+        int pass;
+        scanf("%d",&pass);
+        if(pass == 1999){
+            printf("Correct\n");
+            break;
+        } else{
+            printf("Wrong\n");
+        }
+    }
+    return 0;
+}
