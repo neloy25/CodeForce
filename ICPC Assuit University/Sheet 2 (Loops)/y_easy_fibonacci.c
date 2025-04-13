bbd5196d7b24d868bc2671b031e976c3a5889e92
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-13 23:50:04 +0600

    Sheet 2: easy fibonacci

---
+++ ICPC Assuit University/Sheet 2 (Loops)/y_easy_fibonacci.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int n;
+    scanf("%d",&n);
+    int st = 0;
+    printf("%d ",st);
+    int end = 1;
+    printf("%d ",end);
+    int sum = st + end;
+
+    for(int i = 2; i < n; i++){
+        printf("%d ",sum);
+        st = end;
+        end = sum;
+        sum = st + end;
+    }
+    return 0;
+}
