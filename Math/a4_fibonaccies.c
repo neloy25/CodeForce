770582c2cf13bdc31b046b6f4c7645bea025d92e
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-18 23:20:23 +0600

    Fibonaccies: Problem fixed

--- Math/a4_fibonaccies.c
+++ Math/a4_fibonaccies.c
@@ -1,23 +1,23 @@
 #include<stdio.h>
 
+
 int main() {
     int t;
     scanf("%d", &t);
-    for (int i = 0; i < t; i++) {
-        int arr[5];
-        for (int i = 0; i < 5; i++) {
-            if(i == 2) continue;
-            scanf("%d", &arr[i]);
-        }
-
-        arr[2] = arr[3] - arr[1];
-
-        int res = 0;
+    for (int j = 0; j < t; j++) {
+        int a1,a2,a3,a4,a5;
+        scanf("%d %d %d %d", &a1,&a2,&a4,&a5);
+        int candidate[3] = {a1 + a2, a4 - a2, a5 - a4};
+        int max_fibo = 0;
         for (int i = 0; i < 3; i++) {
-            if(arr[i + 2] == arr[i + 1] + arr[i]) res++;
+            int count = 0;
+            a3 = candidate[i];
+            if(a1 + a2 == a3) count++;
+            if(a2 + a3 == a4) count++;
+            if(a3 + a4 == a5) count++;
+            if(max_fibo < count) max_fibo = count;
         }
-
-        printf("%d\n", res);
+        printf("%d\n", max_fibo);
     }
     return 0;
 }
