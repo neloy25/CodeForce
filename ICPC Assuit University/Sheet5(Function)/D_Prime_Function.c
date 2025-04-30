bf1ed96ccd2713c7f369308b4c97de5568f9490a
neloy25 <sadmansafinneloy678@gmail.com>
2025-04-30 09:55:07 +0600

    Sheet 5 D: prime check error fixed

--- ICPC Assuit University/Sheet5(Function)/D_Prime_Function.c
+++ ICPC Assuit University/Sheet5(Function)/D_Prime_Function.c
@@ -2,6 +2,7 @@
 #include<math.h>
 
 int primeCheck(int n){
+    if(n < 2) return 0;
     int len = sqrt(n);
     for (int i = 2; i <= len; i++) {
         if(n % i == 0){
@@ -12,8 +13,13 @@ int primeCheck(int n){
 }
 
 int main() {
-    int n;
-    scanf("%d", &n);
-    printf("%d", primeCheck(n));
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        if(primeCheck(n)) printf("YES\n");
+        else printf("NO\n");
+    }
     return 0;
 }
