1a2cdf2914ad7b29a1ef3b44f4de61014c76b468
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-02 12:53:54 +0600

    Sheet 5 F: Equation

--- ICPC Assuit University/Sheet5(Function)/F_equation.c
+++ ICPC Assuit University/Sheet5(Function)/F_equation.c
@@ -1,21 +1,19 @@
-// #include<stdio.h>
-// long long int helper(int n, long long int x) {
-//     long long int y = x * x;
-//     long long int result = y;
-//     int times = (n - 2) / 2;
-//     for (int i = 0; i < times; i++) {
-//         result *= (1 + y);
-//     }
-//     return result;
-// }
-// int main() {
-//     int n;
-//     long long int x;
-//     scanf("%lld%d", &x,&n);
-//     if (n < 2){
-//         printf("0");
-//     }else {        
-//         printf("%lld", helper(n, x));
-//     }
-//     return 0;
-// }
+#include<stdio.h>
+long long int power(int n,int e){
+    long long int y = 1;
+    for (int i = 0; i < e; i++) {
+        y *= n;
+    }
+    return y;
+}
+int main() {
+    int x,n;
+    scanf("%d%d", &x,&n);
+    long long int sum = 0;
+    for (int i = 2; i <= n; i+=2) {
+        long long int val = power(x,i);
+        sum+=val;
+    }
+    printf("%lld", sum);
+    return 0;
+}
