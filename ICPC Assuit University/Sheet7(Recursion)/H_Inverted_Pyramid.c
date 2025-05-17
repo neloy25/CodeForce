b0eefae2b9c964244d7b8975474b76ddacda8762
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-17 22:10:31 +0600

    Sheet 7: Inverted pyramid

---
+++ ICPC Assuit University/Sheet7(Recursion)/H_Inverted_Pyramid.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void pyramid(int n,int i,int star,int space){
+    for (int j = 1; j <= space; j++) {
+        printf(" ");
+    }
+    for (int j = 1;j <= star; j++) {
+        printf("*");
+    }
+    if(i == n) return;
+    printf("\n");
+    star-=2;
+    space++;
+    pyramid(n,i+1,star,space);
+}
+
+
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    pyramid(n,1,2*n-1,0);
+    return 0;
+}
