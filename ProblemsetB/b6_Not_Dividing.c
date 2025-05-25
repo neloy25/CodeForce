e7b3dae8ea423033157a6eb71ef3bff2192697d5
neloy25 <sadmansafinneloy678@gmail.com>
2025-05-25 18:27:40 +0600

    Not dividing

---
+++ ProblemsetB/b6_Not_Dividing.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+    while (t--) {
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+
+        for (int i = 0; i < n -1; i++) {
+            if(arr[i] == 1) {
+                arr[i]++;
+                if(i !=0 && arr[i] % arr[i - 1] == 0){
+                    arr[i]++;
+                }
+            }
+            if(arr[i + 1] % arr[i] == 0) arr[i+1]++;
+        }
+
+        for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
